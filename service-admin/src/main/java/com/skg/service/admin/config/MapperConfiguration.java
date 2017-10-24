e49cdb3b0e7d38e8bd70df3298d23d2864c5483f
shikuigang <slting12@163.com>
2017-10-24 09:56:15 +0800

    11

--- service-admin/src/main/java/com/skg/service/admin/config/MapperConfiguration.java
+++ service-admin/src/main/java/com/skg/service/admin/config/MapperConfiguration.java
@@ -9,13 +9,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.core.env.Environment;
 import tk.mybatis.spring.mapper.MapperScannerConfigurer;
 
-/**
- * mybatis mapper 扫描配置类
- * 
- * @author wanghaobin
- * @date 2016年12月15日
- * @since 1.7
- */
+
 @Configuration
 @AutoConfigureAfter(MybatisConfiguration.class)
 public class MapperConfiguration implements EnvironmentAware {
